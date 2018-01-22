0a6341db25aa744e91f4e12837f62e9430f4a545
sky800719 <23828728@qq.com>
2018-01-22 09:54:37 +0800

    test

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
+echo "Kill Exists Python Process!!!"
 ps -ef | grep "manage.py" | grep -v grep | awk '{print $2}' | xargs kill -9
 
+echo "Start Python Server at Port 8000"
 nohup python manage.py runserver sqlaudit:8000 &
 python manage.py crontab add
